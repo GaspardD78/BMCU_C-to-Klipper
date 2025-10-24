ae31667195f03328f6b179e987aef4147563ca00
GaspardD78 <43726973+GaspardD78@users.noreply.github.com>
2025-10-24 17:22:19 +0200

    Add AT8236 virtual drivers and RS-485 tooling

--- klipper-master/klipper-master/src/ch32v20x/gpio.c
+++ klipper-master/klipper-master/src/ch32v20x/gpio.c
@@ -9,12 +9,21 @@
 #include "command.h" // DECL_ENUMERATION_RANGE, shutdown
 #include "compiler.h"
 #include "gpio.h"
+#include "pins_bmcu_c.h"
 
 DECL_ENUMERATION_RANGE("pin", "PA0", GPIO('A', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PB0", GPIO('B', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PC0", GPIO('C', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PD0", GPIO('D', 0), 16);
 DECL_ENUMERATION_RANGE("pin", "PE0", GPIO('E', 0), 16);
+DECL_ENUMERATION("pin", "AT8236_M1_STEP", BMCU_C_MOTOR1_STEP);
+DECL_ENUMERATION("pin", "AT8236_M1_DIR", BMCU_C_MOTOR1_DIR);
+DECL_ENUMERATION("pin", "AT8236_M2_STEP", BMCU_C_MOTOR2_STEP);
+DECL_ENUMERATION("pin", "AT8236_M2_DIR", BMCU_C_MOTOR2_DIR);
+DECL_ENUMERATION("pin", "AT8236_M3_STEP", BMCU_C_MOTOR3_STEP);
+DECL_ENUMERATION("pin", "AT8236_M3_DIR", BMCU_C_MOTOR3_DIR);
+DECL_ENUMERATION("pin", "AT8236_M4_STEP", BMCU_C_MOTOR4_STEP);
+DECL_ENUMERATION("pin", "AT8236_M4_DIR", BMCU_C_MOTOR4_DIR);
 
 static GPIO_TypeDef * const digital_regs[] = {
     ['A' - 'A'] = GPIOA,
@@ -23,6 +32,38 @@ static GPIO_TypeDef * const digital_regs[] = {
     ['D' - 'A'] = GPIOD,
     ['E' - 'A'] = GPIOE,
 };
+
+#define AT8236_VIRTUAL_BASE     BMCU_C_AT8236_PIN_BASE
+#define AT8236_VIRTUAL_MAX      (AT8236_VIRTUAL_BASE \
+                                 + BMCU_C_AT8236_PIN_STRIDE * 4)
+
+struct at8236_channel {
+    struct gpio_out high_pin;
+    struct gpio_out low_pin;
+    uint8_t configured;
+    uint8_t step_state;
+    uint8_t dir_state;
+};
+
+static struct at8236_channel at8236_channels[4];
+
+static inline int
+is_at8236_virtual(uint32_t pin)
+{
+    return pin >= AT8236_VIRTUAL_BASE && pin < AT8236_VIRTUAL_MAX;
+}
+
+static inline uint8_t
+at8236_index(uint32_t pin)
+{
+    return (pin - AT8236_VIRTUAL_BASE) / BMCU_C_AT8236_PIN_STRIDE;
+}
+
+static inline uint8_t
+at8236_role(uint32_t pin)
+{
+    return (pin - AT8236_VIRTUAL_BASE) & 0x1;
+}
 
 static void
 gpio_clock_enable(GPIO_TypeDef *regs)
@@ -86,38 +127,155 @@ gpio_init(void)
     }
 }
 
-struct gpio_out
-gpio_out_setup(uint32_t pin, uint32_t val)
+static void
+gpio_out_write_hw(struct gpio_out g, uint32_t val)
 {
-    GPIO_TypeDef *regs = gpio_pin_to_regs(pin);
-    gpio_clock_enable(regs);
-    struct gpio_out g = { .regs = regs, .bit = GPIO2BIT(pin) };
-    gpio_out_reset(g, val);
-    return g;
-}
-
-void
-gpio_out_reset(struct gpio_out g, uint32_t val)
-{
-    irqstatus_t flag = irq_save();
     if (val)
         g.regs->BSHR = g.bit;
     else
         g.regs->BCR = g.bit;
+}
+
+static void
+gpio_out_toggle_noirq_hw(struct gpio_out g)
+{
+    g.regs->OUTDR ^= g.bit;
+}
+
+static void
+gpio_out_reset_hw(struct gpio_out g, uint32_t val)
+{
+    irqstatus_t flag = irq_save();
+    gpio_out_write_hw(g, val);
     uint32_t pin = 0;
     for (size_t i = 0; i < ARRAY_SIZE(digital_regs); i++)
         if (digital_regs[i] == g.regs) {
             pin = GPIO('A' + i, __builtin_ctz(g.bit));
             break;
         }
-    gpio_peripheral(pin, GPIO_CONFIG(GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_GP_PUSHPULL), 0);
+    gpio_peripheral(pin,
+                    GPIO_CONFIG(GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_GP_PUSHPULL),
+                    0);
     irq_restore(flag);
+}
+
+static struct gpio_out
+gpio_out_setup_hw(uint32_t pin, uint32_t val)
+{
+    GPIO_TypeDef *regs = gpio_pin_to_regs(pin);
+    gpio_clock_enable(regs);
+    struct gpio_out g = { .regs = regs, .bit = GPIO2BIT(pin) };
+    gpio_out_reset_hw(g, val);
+    return g;
+}
+
+static void
+at8236_apply(struct at8236_channel *ch)
+{
+    if (!ch->configured)
+        return;
+    if (ch->step_state) {
+        if (ch->dir_state) {
+            gpio_out_write_hw(ch->high_pin, 0);
+            gpio_out_write_hw(ch->low_pin, 1);
+        } else {
+            gpio_out_write_hw(ch->high_pin, 1);
+            gpio_out_write_hw(ch->low_pin, 0);
+        }
+    } else {
+        gpio_out_write_hw(ch->high_pin, 0);
+        gpio_out_write_hw(ch->low_pin, 0);
+    }
+}
+
+static void
+at8236_configure(uint8_t idx)
+{
+    struct at8236_channel *ch = &at8236_channels[idx];
+    if (ch->configured)
+        return;
+    static const uint32_t high_map[] = {
+        BMCU_C_MOTOR1_HIGH,
+        BMCU_C_MOTOR2_HIGH,
+        BMCU_C_MOTOR3_HIGH,
+        BMCU_C_MOTOR4_HIGH,
+    };
+    static const uint32_t low_map[] = {
+        BMCU_C_MOTOR1_LOW,
+        BMCU_C_MOTOR2_LOW,
+        BMCU_C_MOTOR3_LOW,
+        BMCU_C_MOTOR4_LOW,
+    };
+    ch->high_pin = gpio_out_setup_hw(high_map[idx], 0);
+    ch->low_pin = gpio_out_setup_hw(low_map[idx], 0);
+    ch->step_state = 0;
+    ch->dir_state = 0;
+    ch->configured = 1;
+}
+
+static struct gpio_out
+at8236_setup_pin(uint32_t pin, uint32_t val)
+{
+    uint8_t idx = at8236_index(pin);
+    struct at8236_channel *ch = &at8236_channels[idx];
+    at8236_configure(idx);
+    if (at8236_role(pin))
+        ch->dir_state = !!val;
+    else
+        ch->step_state = !!val;
+    at8236_apply(ch);
+    struct gpio_out g = { .regs = NULL, .bit = pin };
+    return g;
+}
+
+static void
+at8236_write(uint32_t pin, uint32_t val)
+{
+    struct at8236_channel *ch = &at8236_channels[at8236_index(pin)];
+    if (at8236_role(pin))
+        ch->dir_state = !!val;
+    else
+        ch->step_state = !!val;
+    at8236_apply(ch);
+}
+
+static void
+at8236_toggle(uint32_t pin)
+{
+    struct at8236_channel *ch = &at8236_channels[at8236_index(pin)];
+    if (at8236_role(pin))
+        ch->dir_state ^= 1;
+    else
+        ch->step_state ^= 1;
+    at8236_apply(ch);
+}
+
+struct gpio_out
+gpio_out_setup(uint32_t pin, uint32_t val)
+{
+    if (is_at8236_virtual(pin))
+        return at8236_setup_pin(pin, val);
+    return gpio_out_setup_hw(pin, val);
+}
+
+void
+gpio_out_reset(struct gpio_out g, uint32_t val)
+{
+    if (!g.regs && is_at8236_virtual(g.bit)) {
+        at8236_write(g.bit, val);
+        return;
+    }
+    gpio_out_reset_hw(g, val);
 }
 
 void
 gpio_out_toggle_noirq(struct gpio_out g)
 {
-    g.regs->OUTDR ^= g.bit;
+    if (!g.regs && is_at8236_virtual(g.bit)) {
+        at8236_toggle(g.bit);
+        return;
+    }
+    gpio_out_toggle_noirq_hw(g);
 }
 
 void
@@ -131,10 +289,11 @@ gpio_out_toggle(struct gpio_out g)
 void
 gpio_out_write(struct gpio_out g, uint32_t val)
 {
-    if (val)
-        g.regs->BSHR = g.bit;
-    else
-        g.regs->BCR = g.bit;
+    if (!g.regs && is_at8236_virtual(g.bit)) {
+        at8236_write(g.bit, val);
+        return;
+    }
+    gpio_out_write_hw(g, val);
 }
 
 struct gpio_in
--- klipper-master/klipper-master/src/ch32v20x/usart.c
+++ klipper-master/klipper-master/src/ch32v20x/usart.c
@@ -1,16 +1,18 @@
-// Minimal USART1 driver for CH32V20x
+// Minimal USART1 driver for CH32V20x with RS-485 half-duplex support
 
 #include "autoconf.h"
 #include "command.h"
 #include "generic/serial_irq.h"
 #include "gpio.h"
 #include "internal.h"
+#include "pins_bmcu_c.h"
 #include "sched.h"
 
 #if CONFIG_CH32V20X_SERIAL1
-  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA10,PA9");
+  DECL_CONSTANT_STR("RESERVE_PINS_serial", "PA10,PA9,PA12");
   #define GPIO_Rx GPIO('A', 10)
   #define GPIO_Tx GPIO('A', 9)
+  #define GPIO_RTS BMCU_C_RS485_DE
   #define USARTx USART1
   #define USARTx_IRQn USART1_IRQn
 #else
@@ -20,6 +22,17 @@
 #define CTLR1_FLAGS (USART_CTLR1_UE | USART_CTLR1_RE | USART_CTLR1_TE \
                      | USART_CTLR1_RXNEIE)
 
+static struct gpio_out rs485_de;
+static uint8_t rs485_initialized;
+
+static inline void
+rs485_set_direction(uint8_t drive_high)
+{
+    if (!rs485_initialized)
+        return;
+    gpio_out_write(rs485_de, drive_high);
+}
+
 void
 USART1_IRQHandler(void)
 {
@@ -28,16 +41,23 @@ USART1_IRQHandler(void)
         serial_rx_byte(USARTx->DATAR);
     if ((sr & USART_STATR_TXE) && (USARTx->CTLR1 & USART_CTLR1_TXEIE)) {
         uint8_t data;
-        if (serial_get_tx_byte(&data))
-            USARTx->CTLR1 = CTLR1_FLAGS;
-        else
+        if (serial_get_tx_byte(&data)) {
+            USARTx->CTLR1 = CTLR1_FLAGS | USART_CTLR1_TCIE;
+        } else {
             USARTx->DATAR = data;
+        }
+    }
+    if ((sr & USART_STATR_TC) && (USARTx->CTLR1 & USART_CTLR1_TCIE)) {
+        USARTx->STATR &= ~USART_STATR_TC;
+        USARTx->CTLR1 = CTLR1_FLAGS;
+        rs485_set_direction(0);
     }
 }
 
 void
 serial_enable_tx_irq(void)
 {
+    rs485_set_direction(1);
     USARTx->CTLR1 = CTLR1_FLAGS | USART_CTLR1_TXEIE;
 }
 
@@ -54,6 +74,9 @@ serial_init(void)
                     GPIO_CONFIG(GPIO_MODE_INPUT, GPIO_CNF_INPUT_PU_PD), 1);
     gpio_peripheral(GPIO_Tx,
                     GPIO_CONFIG(GPIO_MODE_OUTPUT_50MHZ, GPIO_CNF_AF_PUSHPULL), 0);
+
+    rs485_de = gpio_out_setup(GPIO_RTS, 0);
+    rs485_initialized = 1;
 
     eclic_enable_interrupt(USARTx_IRQn, 1, 2);
 }
